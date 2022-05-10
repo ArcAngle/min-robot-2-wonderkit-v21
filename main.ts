b576f35c1f9a90824f90793ff6921a199dc0c061
ArcAngle <eddy@3os.mobi>
2022-05-10 21:21:48 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P2, 0)
+pins.servoWritePin(AnalogPin.P0, 30)
+basic.forever(function () {
+    if (input.lightLevel() < 60 || input.buttonIsPressed(Button.A)) {
+        for (let index = 0; index < 6; index++) {
+            pins.digitalWritePin(DigitalPin.P2, 1)
+            pins.servoWritePin(AnalogPin.P0, 65)
+            basic.showIcon(IconNames.Heart)
+            basic.pause(200)
+            pins.digitalWritePin(DigitalPin.P2, 0)
+            pins.servoWritePin(AnalogPin.P0, 10)
+            basic.pause(200)
+            basic.showIcon(IconNames.SmallHeart)
+        }
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        pins.servoWritePin(AnalogPin.P0, 30)
+        basic.showIcon(IconNames.Happy)
+    }
+})
